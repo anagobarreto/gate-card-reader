19a2030fef6849298b25e4ccf4658361510fe332
Anacgbarreto <anacgbarreto@gmail.com>
2019-10-30 22:56:43 -0700

    basic first page done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./images/logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Gate error code:</p>
+        <input className="inputbox" maxlength="3"></input>
       </header>
     </div>
   );
